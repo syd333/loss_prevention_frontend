bb8f4ee684778dcc6bd37a660f4f0fcc356cb64a
syd333 <sydmiranda@icloud.com>
2021-08-03 10:42:03 -0400

    app.js onlogin + useeffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { api } from "./services/Api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          LP
-      </header>
-    </div>
-  );
+const App = () => {
+  const [auth, setAuth] = useState({ user: {} });
+
+
+  useEffect(() => {
+    const token = localStorage.token;
+    if (token && token != undefined) {
+      api.auth.getCurrentUser().then((data) => {
+        setAuth({
+          user: {
+            id: data.user.id,
+            username: data.user.username,
+          },
+        });
+      })
+    }
+  }, [])
+
+  const onLogin = (data, routerProps) => {
+    if (data.jwt){
+      console.log('successfully logged in!');
+      localStorage.setItem("token", data.jwt);
+      setAuth({
+        user: {
+          id: data.user.id,
+          username: data.user.username,
+        },
+      });
+    }
+  }
+
 }
-
-export default App;
